f5379a2c9347e10148d9dd5198ea27bf62e2a975
Navdeep Handa <navhan@msn.com>
2019-03-28 14:58:05 -0400

    we donneee

--- nothing_but_the_truth.rb
+++ nothing_but_the_truth.rb
@@ -1,22 +1,64 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def table_and(n, bits, true_symbol, false_symbol)
+  if n == 2**bits - 1
+    return true_symbol
+  end
+  return false_symbol
+end
+
+def table_or(n, bits, true_symbol, false_symbol)
+  if n == 0
+    return false_symbol
+  end
+  return true_symbol
+end
+
+def table_nand(n, bits, true_symbol, false_symbol)
+  if(table_and(n, bits, true_symbol, false_symbol) == true_symbol)
+    return false_symbol
+  end
+  return true_symbol
+end
+
+def table_nor(n, bits, true_symbol, false_symbol)
+  if(table_or(n, bits, true_symbol, false_symbol) == true_symbol)
+    return false_symbol
+  end
+  return true_symbol
+end
+
 def gen_binary_array_representation(n, bits, true_symbol, false_symbol)
   a = []
-  bits.times { |shift| a.push("#{(n >> shift) & 1 == 0 ? true_symbol : false_symbol} ") }
+  bits.times { |shift| a.push("#{(n >> shift) & 1 == 1 ? true_symbol : false_symbol} ") }
   a.reverse
 end
 
 def table(n, true_symbol, false_symbol)
   arr = []
-  (2**n).times do |row|
+  (0..2**n-1).each do |row|
     a = gen_binary_array_representation(row, n, true_symbol, false_symbol)
+    a.push("#{table_and(row, n, true_symbol, false_symbol)} ")
+    a.push("#{table_or(row, n, true_symbol, false_symbol)} ")
+    a.push("#{table_nand(row, n, true_symbol, false_symbol)} ")
+    a.push("#{table_nor(row, n, true_symbol, false_symbol)} ")
     arr.push(a)
   end
-  arr.reverse
+  arr
 end
 
-def and
+def valid_args?(true_symbol, false_symbol, size)
+  if true_symbol.length > 1 || false_symbol.length > 1
+    return false
+  end
+  if true_symbol == false_symbol
+    return false
+  end
+  if size < 2
+    return false
+  end
+  return true
 end
 
 # ****************************************
@@ -36,11 +78,28 @@ get '/' do
 end
 
 get '/display' do
-  true_symbol = params['true_symbol']
-  false_symbol = params['false_symbol']
-  size = params['size'].to_i
+  true_symbol = params['true_symbol'] || "T"
+  false_symbol = params['false_symbol'] || "F"
+  size = params['size']
 
-  truth_table = table(size, true_symbol, false_symbol)
+  if true_symbol.strip.empty?
+    true_symbol = "T"
+  end
+  if false_symbol.strip.empty?
+    false_symbol = "F"
+  end
+  if size.nil?
+    size = 3
+  elsif size.strip.empty?
+    size = 3
+  end
 
-  erb :display, :locals => { size: size, truth_table: truth_table }
+  size = size.to_i
+
+  if valid_args?(true_symbol, false_symbol, size)
+    truth_table = table(size, true_symbol, false_symbol)
+    erb :display, :locals => { size: size, truth_table: truth_table }
+  else
+    erb :error_invalid_params
+  end
 end
